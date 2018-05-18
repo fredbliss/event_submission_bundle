e9dcf1509207ebbdba5c363cf67b8f4216ca72af
fbliss <fred.bliss@intelligentspark.com>
2018-05-18 14:01:58 -0400

    removed datepair

--- src/Resources/public/gulpfile.js
+++ src/Resources/public/gulpfile.js
@@ -16,8 +16,6 @@ var path = {
     js: [
       'node_modules/jquery-ui/ui/widget.js'
       ,'node_modules/jquery-ui/ui/widgets/datepicker.js'
-      ,'node_modules/datepair.js/dist/datepair.js'
-      ,'node_modules/datepair.js/dist/jquery.datepair.js'
       ,'node_modules/timepicker/jquery.timepicker.js'
       ,'node_modules/moment/moment.js'
     ]
