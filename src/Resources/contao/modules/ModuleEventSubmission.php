4f6a0a34e563a762c6d699aefdbae49c67104d3b
fbliss <fred.bliss@intelligentspark.com>
2018-06-21 14:26:44 -0400

    fixed date format incompatibility

--- src/Resources/contao/modules/ModuleEventSubmission.php
+++ src/Resources/contao/modules/ModuleEventSubmission.php
@@ -56,7 +56,7 @@ class ModuleEventSubmission extends Contao_Events
         $GLOBALS['TL_JAVASCRIPT'][] = $assetsDir . '/event-submission.min.js|static';
         $GLOBALS['TL_CSS'][] = $assetsDir . '/event-submission.min.css|static';
 
-        $strDateFormat = (strpos($GLOBALS['TL_CONFIG']['dateFormat'], 'Y')!==false ? str_replace('Y', 'YYYY', $GLOBALS['TL_CONFIG']['dateFormat']) : $GLOBALS['TL_CONFIG']['dateFormat']);
+        $strDateFormat = (strpos($GLOBALS['TL_CONFIG']['dateFormat'], 'Y')!==false ? str_replace('Y', 'yy', $GLOBALS['TL_CONFIG']['dateFormat']) : $GLOBALS['TL_CONFIG']['dateFormat']);
 
         $GLOBALS['TL_JQUERY'][] = "<script> jQuery(document).ready(function(){
                     (function($) {
